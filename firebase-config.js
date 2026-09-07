57726536ec1319d334d6b9ed617f83b01741389c
ICELOLLYjp <c22w6f9b7m@privaterelay.appleid.com>
2026-09-07 23:55:54 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,16 @@
 export const APP_CONFIG = {
-  // Firebase設定が終わるまでは true のままで構いません。
-  // true: localStorageを使うデモモード
-  // false: Firebase Authentication + Firestore
   demoMode: false,
 
-firebase: {
-  apiKey: "AIzaSyA1AZlnCYr5cfHG3HDcyvG17jokBnr5lO8",
-  authDomain: "t-shirtstock.firebaseapp.com",
-  projectId: "t-shirtstock",
-  storageBucket: "t-shirtstock.firebasestorage.app",
-  messagingSenderId: "485805702075",
-  appId: "1:485805702075:web:f9d8668ca57d9f58c4229e",
-  measurementId: "G-TXEHB9FGSR"
-},
+  firebase: {
+    apiKey: "AIzaSyA1AZlnCYr5cfHG3HDcyvG17jokBnr5lO8",
+    authDomain: "t-shirtstock.firebaseapp.com",
+    projectId: "t-shirtstock",
+    storageBucket: "t-shirtstock.firebasestorage.app",
+    messagingSenderId: "485805702075",
+    appId: "1:485805702075:web:f9d8668ca57d9f58c4229e"
+  },
 
   sizes: ["S", "M", "L", "XL", "XXL"],
 
-  // Pinkoi商品名の基本ルール
-  // デザイン名 + ボディ名
   titleOrder: ["design", "body"]
 };
