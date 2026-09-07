05535848efa98b76f5cadc4f87ea5dba147ddefe
ICELOLLYjp <c22w6f9b7m@privaterelay.appleid.com>
2026-09-07 23:52:18 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,12 +5,13 @@ export const APP_CONFIG = {
   demoMode: false,
 
 firebase: {
-  apiKey: "以前と同じもの",
+  apiKey: "AIzaSyA1AZlnCYr5cfHG3HDcyvG17jokBnr5lO8",
   authDomain: "t-shirtstock.firebaseapp.com",
   projectId: "t-shirtstock",
   storageBucket: "t-shirtstock.firebasestorage.app",
   messagingSenderId: "485805702075",
-  appId: "1:485805702075:web:f9d8668ca57d9f58c4229e"
+  appId: "1:485805702075:web:f9d8668ca57d9f58c4229e",
+  measurementId: "G-TXEHB9FGSR"
 },
 
   sizes: ["S", "M", "L", "XL", "XXL"],
